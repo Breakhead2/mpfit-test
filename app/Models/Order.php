c40d2e609c4b8439d6005b255e04821ab99a0df1
Denis Sazonov <81635087+Breakhead2@users.noreply.github.com>
2025-03-13 21:50:06 +0400

    feat: update model relationshops

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'full_name',
+        'order_date',
+        'status',
+        'comment',
+        'quantity',
+        'total_price',
+    ];
+
+    public function products():BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
